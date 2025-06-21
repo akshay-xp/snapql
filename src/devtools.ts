e65166e34735e842bdc4006edc38ad3ed095e85d
Akshay PS <psakshay2022@gmail.com>
2025-06-21 15:15:51 +0530

    setup for chrome devtools

---
+++ src/devtools.ts
@@ -0,0 +1,3 @@
+chrome.devtools.panels.create("GQL Network", "", "panel.html", function () {
+  console.log("GQL Network panel created!")
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "node:path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        devtools: path.resolve(__dirname, "devtools.html"),
+        panel: path.resolve(__dirname, "panel.html"),
+        devtoolsScript: path.resolve(__dirname, "src/devtools.ts"),
+        main: path.resolve(__dirname, "src/main.tsx"),
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 })
