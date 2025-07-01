5ba00c4c0916d5853c87ce3ca422b4f7363a6b28
Akshay PS <psakshay2022@gmail.com>
2025-07-01 22:43:32 +0530

    update version to 0.1.0

--- src/devtools.ts
+++ src/devtools.ts
@@ -1,3 +1,3 @@
-chrome.devtools.panels.create("GQL Network", "", "panel.html", function () {
+chrome.devtools.panels.create("SnapQL", "", "panel.html", function () {
   console.log("GQL Network panel created!")
 })
