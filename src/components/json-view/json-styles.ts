ddd65483ec70de51b7a7371ce1cc5210134373b4
Akshay PS <psakshay2022@gmail.com>
2025-07-26 21:29:36 +0530

    remove extra padding around json tree

--- src/components/json-view/json-styles.ts
+++ src/components/json-view/json-styles.ts
@@ -2,7 +2,7 @@ import type { StyleProps } from "react-json-view-lite/dist/DataRenderer"
 
 export const tailwindStyles: StyleProps = {
   container:
-    "bg-base-100 whitespace-pre-wrap wrap-break-word text-sm p-4 rounded font-mono",
+    "bg-base-100 whitespace-pre-wrap wrap-break-word text-sm rounded font-mono",
   basicChildStyle: "pl-4",
   childFieldsContainer: "",
   label: "text-primary font-semibold mr-1",
